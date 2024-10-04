71e431b64f3b0fbe8be76b174d05258526d69abc
Santino <srossiprieto@gmail.com>
2024-10-04 20:19:09 -0300

    feat: menu-responsive

--- src/components/Navbar/OpenComponents/MenuResponsive.tsx
+++ src/components/Navbar/OpenComponents/MenuResponsive.tsx
@@ -1,5 +1,7 @@
-import { X } from "lucide-react";
+import { ChevronRight, X } from "lucide-react";
 import { Link } from "react-router-dom";
+import ScanOrderly from "../../../assets/ScanOrderly";
+import ButtonPrimary from "../../Ui/ButtonPrimary/ButtonPrimary";
 
 interface MenuResponsiveProps {
   isMenuOpen: boolean;
@@ -11,16 +13,19 @@ function MenuResponsive({ isMenuOpen, handleMenuClose }: MenuResponsiveProps) {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex"
+      className="fixed inset-0 z-50 flex items-center justify-center"
       onClick={handleMenuClose}
     >
       <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm" />
       <div
-        className="relative min-h-dvh inset-y-0 right-0 w-64 bg-black text-gray-300 z-50 flex flex-col transform transition-transform duration-300"
+        className="relative w-full h-full bg-gray-black text-gray-300 z-50 flex flex-col transform transition-transform duration-300"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex justify-between items-center p-3 border-b border-[#25252B]">
-          <h2 className="text-lg font-semibold">Menu</h2>
+        <div className="flex justify-between items-center px-3 py-3 border-b border-[#25252B]">
+          <div className="flex justify-center items-center gap-2">
+            <ScanOrderly className="w-5 h-5" />
+            <h2 className="text-lg font-semibold">Menu</h2>
+          </div>
           <button
             onClick={handleMenuClose}
             className="text-gray-300 hover:text-white focus:outline-none"
@@ -28,37 +33,45 @@ function MenuResponsive({ isMenuOpen, handleMenuClose }: MenuResponsiveProps) {
             <X className="w-6 h-6" />
           </button>
         </div>
-        <div className="flex flex-col p-4 space-y-2">
+        <div className="flex flex-col space-y-2 justify-start w-full text-xl flex-grow">
           <Link
             to="/"
-            className="block px-4 py-2 hover:bg-gray-600 hover:text-white rounded-md"
+            className="flex justify-between items-center px-4 py-2 hover:bg-gray-600 hover:text-white border-b border-[#25252B]"
           >
-            Home
+            <span>Home</span>
+            <ChevronRight className="w-5 h-5" />
           </Link>
           <Link
             to="/about"
-            className="block px-4 py-2 hover:bg-gray-600 hover:text-white rounded-md"
+            className="flex justify-between items-center px-4 py-2 hover:bg-gray-600 hover:text-white border-b border-[#25252B]"
           >
-            About
+            <span>About</span>
+            <ChevronRight className="w-5 h-5" />
           </Link>
           <Link
             to="/contact"
-            className="block px-4 py-2 hover:bg-gray-600 hover:text-white rounded-md"
+            className="flex justify-between items-center px-4 py-2 hover:bg-gray-600 hover:text-white border-b border-[#25252B]"
           >
-            Contact
+            <span>Contact</span>
+            <ChevronRight className="w-5 h-5" />
           </Link>
           <Link
             to="/services"
-            className="block px-4 py-2 hover:bg-gray-600 hover:text-white rounded-md"
+            className="flex justify-between items-center px-4 py-2 hover:bg-gray-600 hover:text-white border-b border-[#25252B]"
           >
-            Services
+            <span>Services</span>
+            <ChevronRight className="w-5 h-5" />
           </Link>
           <Link
             to="/pricing"
-            className="block px-4 py-2 hover:bg-gray-600 hover:text-white rounded-md"
+            className="flex justify-between items-center px-4 py-2 hover:bg-gray-600 hover:text-white border-b border-[#25252B]"
           >
-            Pricing
+            <span>Pricing</span>
+            <ChevronRight className="w-5 h-5" />
           </Link>
+        </div>
+        <div className="p-4">
+          <ButtonPrimary text="Contactanos"/>
         </div>
       </div>
     </div>
--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 const Skeleton: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="animate-pulse p-4 space-y-4 w-full max-w-md">
-        <div className="h-4 bg-gray-200 rounded w-3/4"></div>
-        <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-        <div className="h-4 bg-gray-200 rounded w-full"></div>
-        <div className="h-4 bg-gray-200 rounded w-5/6"></div>
+    <div className="flex mt-24 p-3 flex-col min-h-[65dvh] max-w-screen-lg mx-auto items-start justify-center text-left md:items-center md:text-center">
+      <div className="flex-col max-w-[1200px] w-full items-start md:items-center">
+        <div className="h-8 bg-[#5b5b5b36] rounded w-1/4 mb-4"></div>
+        <div className="gap-3 flex flex-col sm:gap-10 justify-between">
+          <div className="h-12 bg-[#5b5b5b36] rounded w-3/4 mx-0 md:mx-auto"></div>
+          <div className="h-4 bg-[#5b5b5b36] rounded w-1/2 mx-0 md:mx-auto"></div>
+          <div className="h-4 bg-[#5b5b5b36] rounded w-full mx-0 md:mx-auto"></div>
+          <div className="h-4 bg-[#5b5b5b36] rounded w-5/6 mx-0 md:mx-auto"></div>
+          <div className="flex gap-2 justify-start md:justify-center">
+            <div className="h-10 bg-[#5b5b5b36] rounded w-1/4"></div>
+            <div className="h-10 bg-[#5b5b5b36] rounded w-1/4"></div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-8 w-full">
+        <div className="h-48 bg-[#5b5b5b36] rounded w-full"></div>
       </div>
     </div>
   );
