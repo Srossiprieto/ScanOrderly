8f78e6a3a96299c0962dd3f1422041ddd87b8f73
Santino <srossiprieto@gmail.com>
2024-10-08 18:22:59 -0300

    feat: animated & svg

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <Background>
     <div className="min-h-screen relative z-1 bg-transparent text-white flex flex-col items-center justify-start  p-4">
-      <div className="max-w-2xl mt-[100px]">
+      <div className="max-w-2xl mt-[50px]">
         <BlogAbout />
       </div>
     </div>
