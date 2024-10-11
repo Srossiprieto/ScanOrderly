38e9d29ba5b7f4e7c3b17ea1482623b59ee0363e
Santino <srossiprieto@gmail.com>
2024-10-11 18:10:11 -0300

    feat: img-home

--- src/components/SoftwareCardHome/SoftwareCardHome.tsx
+++ src/components/SoftwareCardHome/SoftwareCardHome.tsx
@@ -1,16 +1,25 @@
 import { BorderBeamDemo } from '../Ui/BorderBeam/BorderBeam';
-import ScanOrderlySoftware from '../../assets/ScanOrderlySoftware.webp';
+// import ScanOrderlySoftware from '../../assets/ScanOrderlySoftware.webp';
+import ShotsDark from '../../assets/shotsDark.png';
 import PhonesSoftware from '../../../Phones.webp';
 
 function SoftwareCardHome() {
   return (
     <>
       <BorderBeamDemo>
-        <div className='hidden md:flex bg-cover mt-7 relative w-full h-full'>
-          <img src={ScanOrderlySoftware} alt="Software" className='w-full h-auto object-cover'/>
+        <div className="hidden md:flex bg-cover md:mt-7 relative w-full h-full min-h-full min-w-full">
+          <img
+            src={ShotsDark}
+            alt="Software"
+            className="w-full h-full object-cover" // Imágen ocupa todo el espacio disponible
+          />
         </div>
-        <div className='flex bg-cover mt-7 relative w-full h-full md:hidden'>
-          <img src={PhonesSoftware} alt="Software" className='w-full h-auto object-cover'/>
+        <div className="flex bg-cover mt-7 relative w-full h-full min-h-full min-w-full md:hidden">
+          <img
+            src={PhonesSoftware}
+            alt="Software"
+            className="w-full h-full object-cover" // Modo móvil ajustado
+          />
         </div>
       </BorderBeamDemo>
     </>
@@ -18,3 +27,4 @@ function SoftwareCardHome() {
 }
 
 export default SoftwareCardHome;
+
--- src/components/Ui/BorderBeam/BorderBeam.tsx
+++ src/components/Ui/BorderBeam/BorderBeam.tsx
@@ -8,13 +8,13 @@ interface BorderBeamDemoProps {
 export function BorderBeamDemo({ children }: BorderBeamDemoProps) {
   return (
     <>
-      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-md border border-[#adadad30] bg-background md:h-[500px]">
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-xl border border-[#adadad30] bg-background md:h-[475px] min-h-full min-w-full">
         <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b bg-clip-text text-center text-8xl font-semibold leading-none text-transparent from-gray-600 to-slate-900/10">
           {children}
         </span>
-        <BorderBeam size={250} duration={12} delay={9} />
+        <BorderBeam size={150} duration={12} delay={9} />
       </div>
-      <div className='shadow-effect'></div>
+      <div className="shadow-effect"></div>
     </>
   );
 }
